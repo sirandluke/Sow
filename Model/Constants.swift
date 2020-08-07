b6e3b94c10c01dbd103f8b4dec0c29e5974e1317
Luke Sirand <lsirand@ucsd.edu>
2020-08-07 12:39:58 -0700

    Added MVC design pattern
    Create constants struct

---
+++ Model/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  StudyBuddy
+//
+//  Created by Luke Sirand on 8/7/20.
+//  Copyright © 2020 Luke Sirand. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    let app_name = "StudyBuddy"
+}
