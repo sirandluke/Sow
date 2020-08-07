2391482a49a449251cabc9571b8a715c715ddb48
Luke Sirand <lsirand@ucsd.edu>
2020-08-07 12:36:11 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyBuddy
+//
+//  Created by Luke Sirand on 8/7/20.
+//  Copyright © 2020 Luke Sirand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
