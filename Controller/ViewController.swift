e7006130b2e028366b53476d7b73b6bb90adc015
Luke Sirand <lsirand@ucsd.edu>
2020-08-07 17:51:39 -0700

    add notification sounds

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -7,14 +7,14 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        titleLabel.text = "Study Buddy"
     }
-
-
 }
 
